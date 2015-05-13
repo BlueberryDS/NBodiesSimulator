e10cd31fc16b892edbfbd0f53bcef16019843102
BlueberryDS <Dong.shi@hotmail.com>
2015-05-13 12:28:17 -0400

    Added Header Files for Resource Manager
    Please use with care, as these have not been sanity checked. Needs to be
    implemented

---
+++ ResManager/Image.h
@@ -0,0 +1,45 @@
+#ifndef IMAGE
+#define IMAGE
+
+class Image{
+	//Implementation fields go here. (please remove these tages when done)
+protected:
+	//please define/store the data type used to hold a pointer/reference to the image here.
+	//This is part of the interface. You do not need to create a setter/getter functions for this.
+public:
+	Image();
+	/*
+	Default Constructor
+	Sets all fields to NULL or as appropriate.
+	*/
+
+	Image(/*insert nessessary arguments*/);
+	/*
+	(rewrite this documentation when done)
+	Initializes a image object so that it's ready to go. Arguments could be the fields of the object
+	for example.
+	This function lets you do nice syntax things like this. Imagine if we had a posn object
+
+	posn a(1,2);
+	*/
+
+	Image(const Image & other);
+	/*
+	Copy Constructor
+	Copys other to itself, during the construction process of the object.
+	e.g
+		Image b;
+		Image a = b;
+	*/
+
+	Image& operator= (const Image & other);
+	/*
+	Assignment operator
+	Copies other to itself, unregistering whatever image was already in the object
+	e.g.
+		Image a("file1"), b("file2");
+		a = b;
+	*/
+};
+
+#endif
