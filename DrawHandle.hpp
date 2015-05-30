80f0e4430aa496c1b52973cdb933d8b38db5d2b5
BlueberryDS <Dong.shi@hotmail.com>
2015-05-30 19:16:40 -0400

    Abstract DrawHandle comments and completition

--- DrawHandle.hpp
+++ DrawHandle.hpp
@@ -3,17 +3,32 @@
 
 class DrawHandle
 {
-	/*
-	This class contains the DrawHandle.
-	*/
-	Image image;
 
 public:
-	DrawHandle();
 	virtual ~DrawHandle();
 
 	virtual const float getX();
+	/*
+	Returns a float containing the X position of the drawing in arbitrary coordinates
+	*/
+
 	virtual const float getY();
+	/*
+	Returns a float containing the Y position of the drawing in arbitrary coordinates
+	*/
+
 	virtual const float getRadius();
+	/*
+	Returns a float representing the size in arbirtrary units.
+	*/
+
+	virtual const float getRotation();
+	/*
+	Returns a float with the rotation in radians
+	*/
+
 	const Image getImage();
+	/*
+	Returns the Image object to be drawn.
+	*/
 };
